841793a887244355bbde4807fc179041321e009c
Aastha Neupane <aastha.pane12@gmail.com>
2021-08-13 16:16:55 -0400

    spacing fixed

--- src/main/java/com/assignment/events/models/Event.java
+++ src/main/java/com/assignment/events/models/Event.java
@@ -29,20 +29,16 @@ public class Event {
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Long id;
-
 	@NotNull(message="Please enter a name for the event!")
 	@Size(min=2, max=255, message="The name must be 2-100 characters long!")
 	private String name;
-
 	@NotNull(message="Please enter a valid date!")
 	@Future(message="You can not add an event with a past or present date!")
 	@DateTimeFormat(pattern="yy-MM-dd")
 	private Date date;
-
 	@NotNull(message="Please enter the event location!")
 	@Size(min=2, message="Location must be at least 2 characters long!")
 	private String location;
-
 	@NotNull(message="Please select the state in which the event is being held!")
 	private String state;
 
@@ -73,21 +69,17 @@ public class Event {
 
 	//    CONSTRUCTORS
 	//    ----------------
-
 	public Event() {
-
 	}
-
 	public Event(String name, Date date, String location, String state) {
 		this.name = name;
 		this.date = date;
 		this.location = location;
 		this.state = state;
 	}
-
+	
 	//	PrePersist and PreUpdate methods
 	//	------------------------------------
-
 	@PrePersist
 	protected void onCreate(){
 		this.createdAt = new Date();
@@ -99,86 +91,64 @@ public class Event {
 
 	//	GETTERS ADN SETTERS
 	//	-----------------------------
-
 	public Long getId() {
 		return id;
 	}
-
 	public void setId(Long id) {
 		this.id = id;
 	}
-
 	public String getName() {
 		return name;
 	}
-
 	public void setName(String name) {
 		this.name = name;
 	}
-
 	public Date getDate() {
 		return date;
 	}
-
 	public void setDate(Date date) {
 		this.date = date;
 	}
-
 	public String getLocation() {
 		return location;
 	}
-
 	public void setLocation(String location) {
 		this.location = location;
 	}
-
 	public String getState() {
 		return state;
 	}
-
 	public void setState(String state) {
 		this.state = state;
 	}
-
 	public User getHost() {
 		return host;
 	}
-
 	public void setHost(User host) {
 		this.host = host;
 	}
-
 	public List<User> getAttendees() {
 		return attendees;
 	}
-
 	public void setAttendees(List<User> attendees) {
 		this.attendees = attendees;
 	}
-
 	public Date getCreatedAt() {
 		return createdAt;
 	}
-
 	public void setCreatedAt(Date createdAt) {
 		this.createdAt = createdAt;
 	}
-
 	public Date getUpdatedAt() {
 		return updatedAt;
 	}
-
 	public void setUpdatedAt(Date updatedAt) {
 		this.updatedAt = updatedAt;
 	}
-
 	public List<Message> getMessages() {
 		return messages;
 	}
-
 	public void setMessages(List<Message> messages) {
 		this.messages = messages;
 	}
-
-
 }
--- src/main/java/com/assignment/events/repositories/EventAttendeeRepository.java
+++ src/main/java/com/assignment/events/repositories/EventAttendeeRepository.java
@@ -9,7 +9,6 @@ import com.assignment.events.models.EventAttendee;
 import com.assignment.events.models.User;
 
 public interface EventAttendeeRepository extends CrudRepository<EventAttendee, Long>{
-
 	EventAttendee save(EventAttendee ea);
 	List<EventAttendee> findAll();
 	void deleteById(Long id);
